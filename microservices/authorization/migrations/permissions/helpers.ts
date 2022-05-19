9f96860ed3d3b8d58c4b42d04c3c53dfe5ccc8bd
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-05-19 10:49:24 +0300

    fix(authorization): sync default permissions path incorrect

--- microservices/authorization/migrations/permissions/helpers.ts
+++ microservices/authorization/migrations/permissions/helpers.ts
@@ -1,17 +1,17 @@
 import fs from 'fs';
 
-const DUMP_PATH_ROOT = 'migrations/permissions/list';
+const DUMP_PATH_ROOT = `${__dirname}/list`;
 const DUMP_PATH_MODELS = `${DUMP_PATH_ROOT}/models`;
 const DUMP_PATH_METHODS = `${DUMP_PATH_ROOT}/methods`;
 
 /**
  * Get dump entities
  */
-const getDumpEntities = (filename: string, path: string): any[] => {
+const getDumpEntities = (filename: string, pathname: string): any[] => {
   let dumpEntities = [];
 
   try {
-    dumpEntities = JSON.parse(fs.readFileSync(`${path}/${filename}.json`).toString());
+    dumpEntities = JSON.parse(fs.readFileSync(`${pathname}/${filename}.json`).toString());
   } catch (e) {
     console.info(`${filename} dump not exist.`);
   }
@@ -22,11 +22,11 @@ const getDumpEntities = (filename: string, path: string): any[] => {
 /**
  * Get dump entities in files
  */
-const getDumpEntitiesInFiles = (path: string): any[] => {
-  const files = fs.readdirSync(path);
+const getDumpEntitiesInFiles = (pathname: string): any[] => {
+  const files = fs.readdirSync(pathname);
 
   return files.reduce((entities, filename) => {
-    const msEntities: never[] = JSON.parse(fs.readFileSync(`${path}/${filename}`).toString());
+    const msEntities: never[] = JSON.parse(fs.readFileSync(`${pathname}/${filename}`).toString());
 
     entities.push(...msEntities);
 
@@ -37,8 +37,8 @@ const getDumpEntitiesInFiles = (path: string): any[] => {
 /**
  * Save entities dump
  */
-const saveDump = (entities: any[], filename: string, path: string): void => {
-  fs.writeFileSync(`${path}/${filename}.json`, JSON.stringify(entities, null, 2));
+const saveDump = (entities: any[], filename: string, pathname: string): void => {
+  fs.writeFileSync(`${pathname}/${filename}.json`, JSON.stringify(entities, null, 2));
 };
 
 export {
