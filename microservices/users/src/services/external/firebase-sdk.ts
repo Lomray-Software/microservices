bb308ae13073dd074d4e3ac2e3a15c2138ed1507
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-09-01 19:01:58 +0200

    fix(users): pass firebase admin lib to init service

--- microservices/users/src/services/external/firebase-sdk.ts
+++ microservices/users/src/services/external/firebase-sdk.ts
@@ -1,11 +1,11 @@
 import { FirebaseSdk } from '@lomray/microservice-helpers';
-import type FirebaseAdmin from 'firebase-admin';
+import FirebaseAdmin from 'firebase-admin';
 import { FIREBASE_CREDENTIAL, FIREBASE_FROM_CONFIG_MS } from '@constants/index';
 
 export type TFirebaseAdmin = typeof FirebaseAdmin;
 
 export default (): Promise<TFirebaseAdmin> => {
-  FirebaseSdk.init({
+  FirebaseSdk.init(FirebaseAdmin, {
     hasConfigMs: Boolean(FIREBASE_FROM_CONFIG_MS),
     credential: FIREBASE_CREDENTIAL,
   });
