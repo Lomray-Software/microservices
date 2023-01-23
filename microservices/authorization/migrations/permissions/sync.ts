bd5bf318d73080d63cb1ef88633a674b394773d6
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-01-23 18:30:39 +0100

    feat(authorization): add sync permissions command

--- microservices/authorization/migrations/permissions/sync.ts
+++ microservices/authorization/migrations/permissions/sync.ts
@@ -1,144 +1,67 @@
-import _ from 'lodash';
-import { createConnection } from 'typeorm';
-import Condition from '@entities/condition';
-import Filter from '@entities/filter';
-import Method from '@entities/method';
-import MethodFilter from '@entities/method-filter';
-import Model from '@entities/model';
-import Role from '@entities/role';
-import UserRole from '@entities/user-role';
-import {
-  DUMP_PATH_ROOT,
-  DUMP_PATH_MODELS,
-  DUMP_PATH_METHODS,
-  getDumpEntities,
-  saveDump,
-} from './helpers';
+import ResolveSrv from '@lomray/microservice-helpers/helpers/resolve-srv';
+import axios from 'axios';
+
+interface ISyncResponse {
+  result: {
+    microservices: Record<string, { isSuccess?: boolean; error?: string }>;
+  };
+}
 
 /**
- * Create or update roles
+ * Get microservices metadata and update schema in DB
  */
-const createOrUpdateRoles = (roles: Role[]): void => {
-  const dumpFilters = getDumpEntities('roles', DUMP_PATH_ROOT);
-  const keepActual = _.intersectionBy(dumpFilters, roles, 'alias');
-  const merged = _.merge(_.keyBy(keepActual, 'alias'), _.keyBy(roles, 'alias'));
-  const values = _.values(merged).map((f) => _.omit(f, ['updatedAt']));
+const syncPermissions = async () => {
+  let domain = process.env.MS_CONNECTION || 'http://127.0.0.1:8001';
+  const isSRV = Boolean(process.env.MS_CONNECTION_SRV ?? false);
 
-  saveDump(values, 'roles', DUMP_PATH_ROOT);
+  if (isSRV) {
+    domain = await ResolveSrv(domain);
+  }
+
+  const data = {
+    id: '199',
+    method: 'service.sync-metadata',
+    params: {
+      defaultSchemaRoles: ['admin'],
+      defaultAllowGroup: ['admin'],
+      commonModelAliases: [
+        'CountRequestParams',
+        'CountOutputParams',
+        'ListRequestParams',
+        'ViewRequestParams',
+        'RemoveRequestParams',
+        'RestoreRequestParams',
+        'MicroserviceMetaOutput',
+        'RemoteConfigOutput',
+        'ClientRegisterMiddlewareInput',
+        'ClientRegisterMiddlewareOutput',
+        'RemoteConfigOutput',
+      ],
+      payload: {
+        isInternal: true,
+      },
+    },
+  };
+
+  try {
+    const result = await axios.request<ISyncResponse>({
+      baseURL: domain,
+      url: '/ms/authorization',
+      method: 'POST',
+      data,
+    });
+
+    console.log('Microservices are synchronized:');
+
+    Object.entries(result?.data?.result?.microservices ?? {}).forEach(([name, res]) => {
+      console.info(
+        res?.isSuccess ? '\x1b[32m%s\x1b[0m' : '\x1b[31m%s\x1b[0m',
+        `${name}: ${String(res.isSuccess ?? res.error)}`,
+      );
+    });
+  } catch (e) {
+    console.log(`Failed to sync permissions: ${e.message as string}`);
+  }
 };
 
-/**
- * Create or update user roles
- */
-const createOrUpdateUserRoles = (userRoles: UserRole[]): void => {
-  const dumpFilters = getDumpEntities('user-roles', DUMP_PATH_ROOT);
-  const keepActual = _.intersectionBy(dumpFilters, userRoles, 'userId');
-  const merged = _.merge(_.keyBy(keepActual, 'userId'), _.keyBy(userRoles, 'userId'));
-  const values = _.values(merged).map((f) => _.omit(f, ['updatedAt']));
-
-  saveDump(values, 'user-roles', DUMP_PATH_ROOT);
-};
-
-/**
- * Create or update filters
- */
-const createOrUpdateFilters = (filters: Filter[]): void => {
-  const dumpFilters = getDumpEntities('filters', DUMP_PATH_ROOT);
-  const keepActual = _.intersectionBy(dumpFilters, filters, 'title');
-  const merged = _.merge(_.keyBy(keepActual, 'title'), _.keyBy(filters, 'title'));
-  const values = _.values(merged).map((f) => _.omit(f, ['id', 'updatedAt']));
-
-  saveDump(values, 'filters', DUMP_PATH_ROOT);
-};
-
-/**
- * Create or update conditions
- */
-const createOrUpdateConditions = (conditions: Condition[]): void => {
-  const dumpFilters = getDumpEntities('conditions', DUMP_PATH_ROOT);
-  const keepActual = _.intersectionBy(dumpFilters, conditions, 'title');
-  const merged = _.merge(_.keyBy(keepActual, 'title'), _.keyBy(conditions, 'title'));
-  const values = _.values(merged).map((f) => _.omit(f, ['updatedAt']));
-
-  saveDump(values, 'conditions', DUMP_PATH_ROOT);
-};
-
-/**
- * Create or update models
- */
-const createOrUpdateModels = (models: Model[]): void => {
-  const groupedModels = _.groupBy(models, (m) => m.microservice || 'common');
-
-  Object.entries(groupedModels).forEach(([microservice, msModels]) => {
-    const dumpModels = getDumpEntities(microservice, DUMP_PATH_MODELS);
-    const keepActual = _.intersectionBy(dumpModels, msModels, 'alias');
-    const merged = _.merge(_.keyBy(keepActual, 'alias'), _.keyBy(msModels, 'alias'));
-    const values = _.values(merged).map((f) => _.omit(f, ['id', 'updatedAt']));
-
-    saveDump(values, microservice, DUMP_PATH_MODELS);
-  });
-};
-
-/**
- * Create or update methods
- */
-const createOrUpdateMethods = (methods: Method[]): void => {
-  const groupedMethods = _.groupBy(methods, (m) => m.microservice || 'common');
-  const uniqueKey = (m: Method) => `${m.microservice}:${m.method}`;
-
-  Object.entries(groupedMethods).forEach(([microservice, msMethods]) => {
-    const dumpMethods = getDumpEntities(microservice, DUMP_PATH_METHODS);
-    const keepActual = _.intersectionBy(dumpMethods, msMethods, uniqueKey);
-    const merged = _.merge(_.keyBy(keepActual, uniqueKey), _.keyBy(msMethods, uniqueKey));
-    const values = _.values(merged)
-      .map((f) => _.omit(f, ['id', 'modelInId', 'modelOutId', 'conditionId', 'updatedAt']))
-      .map(({ modelIn, modelOut, methodFilters, condition, ...method }) => ({
-        ...method,
-        modelIn: modelIn?.alias,
-        modelOut: modelOut?.alias,
-        condition: condition?.title,
-        methodFilters: methodFilters
-          ?.map((f: MethodFilter) => _.omit(f, ['methodId', 'filterId', 'updatedAt']))
-          // @ts-ignore
-          .map(({ filter, ...fields }) => ({
-            filterTitle: filter.title,
-            ...fields,
-          })),
-      }));
-
-    saveDump(values, microservice, DUMP_PATH_METHODS);
-  });
-};
-
-/**
- * Get permission from DB and make json default permission files for migration
- */
-const sync = async () => {
-  const connection = await createConnection();
-  const methodRepo = connection.getRepository(Method);
-  const modelRepo = connection.getRepository(Model);
-  const filtersRepo = connection.getRepository(Filter);
-  const rolesRepo = connection.getRepository(Role);
-  const userRolesRepo = connection.getRepository(UserRole);
-  const conditionRepo = connection.getRepository(Condition);
-
-  const filters = await filtersRepo.find();
-  const roles = await rolesRepo.find();
-  const condition = await conditionRepo.find();
-  const userRoles = await userRolesRepo.find();
-  const models = await modelRepo.find();
-  const methods = await methodRepo.find({
-    relations: ['modelOut', 'modelIn', 'methodFilters', 'methodFilters.filter', 'condition'],
-  });
-
-  createOrUpdateRoles(roles);
-  createOrUpdateUserRoles(userRoles);
-  createOrUpdateFilters(filters);
-  createOrUpdateConditions(condition);
-  createOrUpdateModels(models);
-  createOrUpdateMethods(methods);
-
-  return connection.close();
-};
-
-export default sync();
+export default syncPermissions();
