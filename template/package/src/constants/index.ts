2346f60105b7f1b7e774b3a19986f622b6d35202
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-01-25 14:05:07 +0100

    fix: package template constants

--- template/package/src/constants/index.ts
+++ template/package/src/constants/index.ts
@@ -1,13 +1,22 @@
-import CONST from '@lomray/microservice-name/constants';
+import GetConstants from '@lomray/microservice-helpers/helpers/get-constants';
+import CONST from '@lomray/microservice-users/constants';
 import { version, name } from '../../package.json';
 
 const isBuild = process.env.__IS_BUILD__;
+
+const { IS_BUILD, SRC_FOLDER } = GetConstants({
+  msNameDefault: CONST.MS_NAME,
+  version,
+  isBuild,
+  packageName: name,
+});
 
 const constants = {
   ...CONST,
   VERSION: version,
   PACKAGE_NAME: name,
-  IS_BUILD: isBuild as unknown as boolean,
+  IS_BUILD,
+  SRC_FOLDER,
   EXTEND_PACKAGE_NAME: CONST.PACKAGE_NAME,
 };
 
