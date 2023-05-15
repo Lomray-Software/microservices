ac206e69aa0f308034b0008681a6065510fed1e3
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-05-15 21:41:14 +0400

    fix: add required validators for card entity

--- microservices/payment/src/entities/card.ts
+++ microservices/payment/src/entities/card.ts
@@ -1,5 +1,5 @@
 import { IsTypeormDate, IsUndefinable } from '@lomray/microservice-helpers';
-import { IsBoolean, IsString, Length } from 'class-validator';
+import { IsBoolean, Length } from 'class-validator';
 import { JSONSchema } from 'class-validator-jsonschema';
 import {
   Entity,
@@ -7,7 +7,6 @@ import {
   ManyToOne,
   UpdateDateColumn,
   CreateDateColumn,
-  Index,
   Column,
   Unique,
   PrimaryColumn,
@@ -30,11 +29,10 @@ class Card {
     description: 'Field for storing id of according card entity',
     example: 'card_1N7zzsFpQjUWTpHeCrYKrCkR',
   })
-  @PrimaryColumn({ type: 'varchar', length: 19 })
+  @PrimaryColumn({ type: 'varchar', length: 29 })
   @Length(1, 29)
   cardId: string;
 
-  @Index('IDX_card_userId', ['userId'])
   @Column({ type: 'varchar', length: 36 })
   @Unique(['userId'])
   @Length(1, 36)
@@ -44,9 +42,9 @@ class Card {
     description: 'Last 4 card digits',
     example: '4242',
   })
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', length: 4 })
   @IsLastCardDigitsValid()
-  @IsString()
+  @Length(1, 4)
   lastDigits: string;
 
   @JSONSchema({
@@ -54,25 +52,26 @@ class Card {
   })
   @Column({ type: 'varchar', length: 5 })
   @IsCardExpirationValid()
-  @Length(5, 5)
+  @Length(1, 5)
   expired: string;
 
-  @JSONSchema({})
-  @Column({ type: 'varchar', length: 255, default: '' })
+  @Column({ type: 'varchar', length: 100, default: '' })
   @IsUndefinable()
-  @Length(1, 255)
+  @Length(1, 100)
   holderName: string;
 
   @JSONSchema({
     example: 'visa',
   })
   @Column({ type: 'varchar', length: 20 })
+  @Length(1, 20)
   type: string;
 
   @JSONSchema({
     description: "If it's the first attached user card it should be default",
   })
   @Column({ type: 'boolean', default: false })
+  @IsUndefinable()
   @IsBoolean()
   isDefault: boolean;
 
