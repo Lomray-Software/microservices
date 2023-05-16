4869b4f8de00f75087db13cfb537521672adf2a5
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-05-16 14:40:58 +0400

    feat: bank account init entity

--- microservices/payment/src/entities/bank-account.ts
+++ microservices/payment/src/entities/bank-account.ts
@@ -1,6 +1,11 @@
-import { Entity } from 'typeorm';
+import { Allow } from 'class-validator';
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-class BankAccount {}
+class BankAccount {
+  @PrimaryGeneratedColumn('uuid')
+  @Allow()
+  id: string;
+}
 
 export default BankAccount;
