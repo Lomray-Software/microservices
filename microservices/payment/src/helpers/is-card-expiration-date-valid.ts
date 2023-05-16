f4a9d935b7bb65f9045fd6f9ca928e043c563d2a
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-05-17 02:25:56 +0400

    feat: update expiration date validation

--- microservices/payment/src/helpers/is-card-expiration-date-valid.ts
+++ microservices/payment/src/helpers/is-card-expiration-date-valid.ts
@@ -9,11 +9,30 @@ const isCardExpirationDateValid = (date: string) => {
     return false;
   }
 
-  const expirationDate = new Date(Number(expirationYear), Number(expirationMonth) - 1, 1);
+  const expirationDate = new Date(
+    getExpirationYear(expirationYear, currentDate),
+    Number(expirationMonth) - 1,
+    1,
+  );
 
   expirationDate.setHours(0, 0, 0, 0);
 
   return expirationDate >= currentDate;
+};
+
+/**
+ * Returns expiration year
+ */
+const getExpirationYear = (expirationYear: string, currentDate: Date): number => {
+  const currentCentury = Math.floor(currentDate.getFullYear() / 100) * 100;
+
+  let parsedExpirationYear = Number(expirationYear);
+
+  if (parsedExpirationYear >= 0 && parsedExpirationYear <= 99) {
+    parsedExpirationYear += currentCentury;
+  }
+
+  return parsedExpirationYear;
 };
 
 export default isCardExpirationDateValid;
