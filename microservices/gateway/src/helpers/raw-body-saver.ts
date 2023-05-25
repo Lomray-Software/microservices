8c1d88701c0f6df3f748208f8aac84cb08b650bd
vackca <48718981+vackca@users.noreply.github.com>
2023-05-25 11:47:28 +0200

    feat: created tests for middleware (#30)
    * feat: created tests for middleware
    
    * feat: created tests for middleware

--- microservices/gateway/src/helpers/raw-body-saver.ts
+++ microservices/gateway/src/helpers/raw-body-saver.ts
@@ -3,10 +3,9 @@ import { IncomingMessage, ServerResponse } from 'http';
 /**
  * Adds raw body to request
  */
-const rawBodySaver = function (req: IncomingMessage, res: ServerResponse, buf: Buffer) {
+const rawBodySaver = function (req: IncomingMessage, res: ServerResponse, buf: Buffer | undefined) {
   if (buf && buf.length) {
-    // @ts-ignore
-    req.rawBody = buf.toString();
+    req['rawBody'] = buf.toString();
   }
 };
 
