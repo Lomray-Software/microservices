2be6d2f2e890e0e37400e16db6537dd8b7b93461
nackca <nackca@gmail.com>
2023-05-26 19:33:39 +0200

    fix: fixed webhook payment according to gateway changes

--- microservices/payment-stripe/src/methods/stripe/webhook.ts
+++ microservices/payment-stripe/src/methods/stripe/webhook.ts
@@ -1,15 +1,16 @@
 import { Endpoint } from '@lomray/microservice-helpers';
 import { IsObject, IsString } from 'class-validator';
+import Stripe from 'stripe';
 import { getManager } from 'typeorm';
 import remoteConfig from '@config/remote';
 import Factory from '@services/payment-gateway/factory';
 
 class WebhookInput {
-  @IsString()
-  body: string;
-
   @IsObject()
-  headers: Record<string, string>;
+  body: Stripe.Event;
+
+  @IsString()
+  rawBody: string;
 }
 
 class WebhookOutput {
@@ -25,16 +26,20 @@ const webhook = Endpoint.custom(
     output: WebhookOutput,
     description: 'Get stripe webhooks handler',
   }),
-  async ({ body, headers }) => {
+  async ({ rawBody, payload }) => {
     const { webhookKey } = await remoteConfig();
 
     if (!webhookKey) {
       throw new Error('Webhook key is not provided');
     }
 
+    if (!payload?.headers?.['stripe-signature']) {
+      throw new Error('Stripe signature is mot provided');
+    }
+
     const service = await Factory.create(getManager());
 
-    service.handleWebhookEvent(body, headers['stripe-signature'], webhookKey);
+    service.handleWebhookEvent(rawBody, payload.headers['stripe-signature'] as string, webhookKey);
 
     return { isHandled: true };
   },
