7d41482c4263dcf0fe624e8cf6a98bc117e904d6
nackca <nackca@gmail.com>
2023-05-26 12:47:40 +0200

    chore: refactored payment to payment-stripe

--- microservices/payment-stripe/src/constants/index.ts
+++ microservices/payment-stripe/src/constants/index.ts
@@ -2,7 +2,7 @@ import GetConstants from '@lomray/microservice-helpers/helpers/get-constants';
 import { version, name } from '../../package.json';
 
 const isBuild = process.env.__IS_BUILD__;
-const msNameDefault = 'payment';
+const msNameDefault = 'payment-stripe';
 
 const constants = {
   ...GetConstants({ msNameDefault, version, isBuild, packageName: name, withDb: true }),
