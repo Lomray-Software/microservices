5e430512fc22cdcc20108cfee433de35301f1712
Andrey Savostinkevich <137174055+a-sava@users.noreply.github.com>
2023-08-14 13:15:43 +0300

    feat(payment-stripe): remove coupon (#71)
    * feat(payment): remove coupon
    
    * fix(payment): review fixes

---
+++ microservices/payment-stripe/src/subscribers/coupon.ts
@@ -0,0 +1,38 @@
+import { BaseException } from '@lomray/microservice-nodejs-lib';
+import { EntitySubscriberInterface, EventSubscriber, RemoveEvent } from 'typeorm';
+import CouponEntity from '@entities/coupon';
+import Factory from '@services/payment-gateway/factory';
+
+/**
+ * Coupon subscriber
+ */
+@EventSubscriber()
+class Coupon implements EntitySubscriberInterface<CouponEntity> {
+  /**
+   * This subscriber only for coupon entity
+   */
+  public listenTo(): typeof CouponEntity {
+    return CouponEntity;
+  }
+
+  /**
+   * Handle coupon remove
+   */
+  public async beforeRemove({ entity, manager }: RemoveEvent<CouponEntity>): Promise<void> {
+    if (!entity?.couponId) {
+      return;
+    }
+
+    const service = await Factory.create(manager);
+    const isDeleted = await service.removeCoupon(entity.couponId);
+
+    if (!isDeleted) {
+      throw new BaseException({
+        status: 500,
+        message: "Can't remove coupon. Please try later.",
+      });
+    }
+  }
+}
+
+export default Coupon;
