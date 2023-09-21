0046bcac7de8c697c465f69672593d5fb438566c
Andrey Savostinkevich <137174055+a-sava@users.noreply.github.com>
2023-09-21 12:22:28 +0300

    feat(notification): hide all notifications (#92)
    * feat(notification): hide all notifications
    
    * fix(notification): review fixes
    
    * fix(notification): review fixes
    
    * fix(notification): review fixes
    
    * fix(notification): review fixes

---
+++ microservices/notification/src/methods/notice/hide-all.ts
@@ -0,0 +1,25 @@
+import { Endpoint, IsUndefinable } from '@lomray/microservice-helpers';
+import { IsBoolean, IsNumber } from 'class-validator';
+import Notice from '@services/notice';
+
+export class HideAllOutput {
+  @IsBoolean()
+  status: boolean;
+
+  @IsNumber()
+  @IsUndefinable()
+  affected?: number;
+}
+
+/**
+ * Hide all user's notifications
+ */
+const hideAll = Endpoint.custom(
+  () => ({
+    output: HideAllOutput,
+    description: 'Notifications multiple hide',
+  }),
+  ({ payload }) => Notice.init().hideAll(payload?.authentication?.userId as string),
+);
+
+export default hideAll;
