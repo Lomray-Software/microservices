79cd98a34d4c93e002f75bb79aaf0799ef065694
vackca <48718981+vackca@users.noreply.github.com>
2023-10-10 13:39:53 +0400

    fix: fixed incorrect amount for payouts when using promocodes(checkout flow) (#95)

--- microservices/payment-stripe/src/services/payment-gateway/stripe.ts
+++ microservices/payment-stripe/src/services/payment-gateway/stripe.ts
@@ -1411,7 +1411,12 @@ class Stripe extends Abstract {
    * Handles completing of transaction inside stripe payment process
    */
   public async handleTransactionCompleted(event: StripeSdk.Event): Promise<Transaction | void> {
-    const { id, payment_status: paymentStatus, status } = event.data.object as ICheckoutEvent;
+    const {
+      id,
+      payment_status: paymentStatus,
+      status,
+      amount_total: amountTotal,
+    } = event.data.object as ICheckoutEvent;
 
     const transaction = await this.transactionRepository.findOne({ transactionId: id });
 
@@ -1423,6 +1428,7 @@ class Stripe extends Abstract {
       { transactionId: id },
       {
         status: this.getStatus(paymentStatus as StripeTransactionStatus),
+        amount: amountTotal,
         params: {
           checkoutStatus: status as StripeCheckoutStatus,
           paymentStatus: paymentStatus as StripeTransactionStatus,
