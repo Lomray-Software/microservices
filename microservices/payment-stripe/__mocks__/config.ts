dc47db0f11a357be823c9d66dbef49c39295315c
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-10-16 20:27:07 +0400

    feat(payment-stripe): add calculation service compute pi tax tests

---
+++ microservices/payment-stripe/__mocks__/config.ts
@@ -0,0 +1,13 @@
+const configMock = {
+  fees: {
+    paymentPercent: 2.9,
+    stableUnit: 30,
+  },
+  taxes: {
+    defaultPercent: 8,
+    stableUnit: 50,
+  },
+};
+
+/* eslint-disable import/prefer-default-export */
+export { configMock };
--- microservices/payment-stripe/__tests__/services/calculation-test.ts
+++ microservices/payment-stripe/__tests__/services/calculation-test.ts
@@ -1,19 +1,16 @@
 import { TypeormMock } from '@lomray/microservice-helpers/mocks';
 import { expect } from 'chai';
 import sinon from 'sinon';
+import type StripeSdk from 'stripe';
+import { configMock } from '@__mocks__/config';
 import * as remoteConfig from '@config/remote';
 import TransactionRole from '@constants/transaction-role';
 import Calculation from '@services/calculation';
 
 describe('services/calculation', () => {
   const sandbox = sinon.createSandbox();
-
-  const remoteConfigStub = sinon.stub().resolves({
-    fees: {
-      paymentPercent: 2.9,
-      stableUnit: 30,
-    },
-  });
+  const mathRoundSpy = sinon.spy(Math, 'round');
+  const remoteConfigStub = sinon.stub().resolves(configMock);
 
   sinon.replace(remoteConfig, 'default', remoteConfigStub);
 
@@ -22,6 +19,7 @@ describe('services/calculation', () => {
   });
 
   afterEach(() => {
+    mathRoundSpy.restore();
     sinon.restore();
     sandbox.restore();
   });
@@ -37,6 +35,7 @@ describe('services/calculation', () => {
         processingAmountUnit: 10330,
         stripeFeeUnit: 330,
       });
+      expect(mathRoundSpy).to.called;
       expect(remoteConfigStub).to.called;
     });
 
@@ -51,6 +50,54 @@ describe('services/calculation', () => {
         stripeFeeUnit: 320,
       });
       expect(remoteConfigStub).to.called;
+    });
+  });
+
+  describe('getPaymentIntentTax', () => {
+    const mockParams = {
+      processingTransactionAmountUnit: 11000,
+      paymentMethodId: 'pm-1',
+      feesPayer: TransactionRole.SENDER,
+    };
+
+    const mockTax = {
+      taxId: 'tax-1',
+    };
+
+    it('should calculate tax and fee correctly when fees are covered by sender', async () => {
+      const computePaymentIntentTaxStub = sinon.stub().resolves(mockTax);
+      const result = await Calculation.getPaymentIntentTax.call(
+        { computePaymentIntentTax: computePaymentIntentTaxStub },
+        {} as StripeSdk,
+        mockParams,
+      );
+
+      const [[, input]] = computePaymentIntentTaxStub.args;
+
+      expect(result).to.deep.equal({ tax: mockTax, feeUnit: configMock.taxes.stableUnit });
+      expect(computePaymentIntentTaxStub).to.calledOnce;
+      expect(input).to.deep.equal({
+        paymentMethodId: mockParams.paymentMethodId,
+        amountUnit: mockParams.processingTransactionAmountUnit + configMock.taxes.stableUnit,
+      });
+    });
+
+    it('should calculate tax and fee correctly when fees are covered by receiver', async () => {
+      const computePaymentIntentTaxStub = sinon.stub().resolves(mockTax);
+      const result = await Calculation.getPaymentIntentTax.call(
+        { computePaymentIntentTax: computePaymentIntentTaxStub },
+        {} as StripeSdk,
+        { ...mockParams, feesPayer: TransactionRole.RECEIVER },
+      );
+
+      const [[, input]] = computePaymentIntentTaxStub.args;
+
+      expect(result).to.deep.equal({ tax: mockTax, feeUnit: configMock.taxes.stableUnit });
+      expect(computePaymentIntentTaxStub).to.calledOnce;
+      expect(input).to.deep.equal({
+        paymentMethodId: mockParams.paymentMethodId,
+        amountUnit: mockParams.processingTransactionAmountUnit,
+      });
     });
   });
 });
