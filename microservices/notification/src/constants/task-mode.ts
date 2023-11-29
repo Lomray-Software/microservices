36dd00bc47274536946a053d4303471978d62c34
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-11-30 02:10:11 +0400

    feat(notification): update jsdocs

--- microservices/notification/src/constants/task-mode.ts
+++ microservices/notification/src/constants/task-mode.ts
@@ -3,8 +3,9 @@ enum TaskMode {
   DEFAULT = 'default',
   /**
    * In this mode tasks will be rechecked before execute, check if exist in db for each chunk
-   * @description Usage: In microservice will down, and for instance, some users will not receive emails
-   * and 3000 users already received email, but task have waiting status without last target error (microservice down).
+   * @description Usage: If whole backend will down, and for instance, some users will not receive emails
+   * and 3000 users already received email, but task have waiting status without last target error,
+   * because whole backed down and notification microservice did not update actual task statuses.
    */
   FULL_CHECK_UP = 'fullCheckUp',
 }
