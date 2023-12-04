306f4305751aa057940afb39a2ec90c182de7ec2
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-12-04 21:16:27 +0400

    feat(payment-stripe): update jsdocs

--- microservices/payment-stripe/src/services/webhook-handlers/index.ts
+++ microservices/payment-stripe/src/services/webhook-handlers/index.ts
@@ -4,6 +4,8 @@ import Dispute from './dispute';
 /**
  * Webhook handlers
  * @description Webhook handlers for Stripe events
+ * Each service related to event object that it represents
+ * @example event.object is Dispute, so Dispute service should handle it, event if route is charge.dispute.created
  * Decomposed logic from payment stripe service
  */
 class WebhookHandlers {
