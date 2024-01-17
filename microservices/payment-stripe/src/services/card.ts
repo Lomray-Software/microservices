c9b2e61ba4dae84253382453d806b319c0750af5
Oleg Drozdovich <44732463+OlegDO@users.noreply.github.com>
2024-01-17 14:31:08 +0100

    feat(payment-stripe): update stripe payout register amount (#151)
    * feat(payment-stripe): update stripe payout register
    
    * feat(payment-stripe): first card set up (#152)
    
    * feat(payment-stripe): update first card set up
    
    * feat(payment-stripe): clean up

--- microservices/payment-stripe/src/services/card.ts
+++ microservices/payment-stripe/src/services/card.ts
@@ -19,7 +19,7 @@ class Card {
    * 1. Card should be created from setup intent succeed
    * 2. Should be called after insert
    */
-  public static async handleCreate(entity: CardEntity, manager: EntityManager): Promise<void> {
+  public static async handleAfterInsert(entity: CardEntity, manager: EntityManager): Promise<void> {
     const paymentMethodId = CardRepository.extractPaymentMethodId(entity);
 
     if (!paymentMethodId) {
@@ -33,7 +33,7 @@ class Card {
 
     const cardRepository = manager.getRepository(CardEntity);
     const customerRepository = manager.getRepository(CustomerEntity);
-    const service = await Factory.create(getManager());
+    const service = await Factory.create(manager);
 
     /**
      * Get attached cards count as the payment method
@@ -41,28 +41,31 @@ class Card {
     const cardsCount = await cardRepository
       .createQueryBuilder('card')
       .where('card.userId = :userId', { userId: entity.userId })
+      // Select cards that attached to connect account (have payment method id)
       .andWhere(
         `card.params ->> 'paymentMethodId' IS NOT NULL OR card."paymentMethodId" IS NOT NULL`,
       )
       .andWhere('card.isDefault = :isDefault', { isDefault: true })
       .getCount();
 
-    /**
-     * Card count will contain current card
-     */
-    if (cardsCount) {
+    // Card count will contain current card, because after update event
+    if (cardsCount > 1) {
       void Microservice.eventPublish(Event.CardCreated, entity);
 
       return;
     }
 
-    /**
-     * If 0 cards with the required params for payment method
-     */
+    // If 1 (current card) with the required params for payment method
     const customer = await customerRepository.findOne({ userId: entity.userId });
 
     if (!customer) {
-      throw new BaseException({ status: 500, message: messages.getNotFoundMessage('Customer') });
+      throw new BaseException({
+        status: 500,
+        message: messages.getNotFoundMessage('Customer'),
+        payload: {
+          userId: entity.userId,
+        },
+      });
     }
 
     const isSet = await service.setDefaultCustomerPaymentMethod(
@@ -73,7 +76,7 @@ class Card {
     if (!isSet) {
       throw new BaseException({
         status: 500,
-        message: 'Failed to set card as the default payment method for customer',
+        message: 'Failed to set card as the default payment method for customer.',
       });
     }
 
