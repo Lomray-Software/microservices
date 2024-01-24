1205bfe17336b960990bd79ea05d7a9e97b02a4d
Oleg Drozdovich <44732463+OlegDO@users.noreply.github.com>
2024-01-24 22:35:13 +0100

    feat(payment-stripe): add card expiration message (#155)
    * feat(payment-stripe): add card expiration message
    
    * feat(payment-stripe): update jsdocs
    
    * feat(payment-stripe): update jsdocs
    
    * Update microservices/payment-stripe/src/helpers/validators/is-card-expiration-valid.ts
    
    Co-authored-by: Mikhail Yarmaliuk <hellboymxb@gmail.com>
    
    ---------
    
    Co-authored-by: Mikhail Yarmaliuk <hellboymxb@gmail.com>

--- microservices/payment-stripe/src/helpers/validators/is-card-expiration-valid.ts
+++ microservices/payment-stripe/src/helpers/validators/is-card-expiration-valid.ts
@@ -8,11 +8,23 @@ import isCardExpirationDateValid from '@helpers/is-card-expiration-date-valid';
 
 /**
  * Card expiration date validator
+ * @description Card expires in the first day of the next month
+ * @example Expiration date 02/23. At the 1st of 03/23 card will be expired
  */
 @ValidatorConstraint({ name: 'isCardExpirationValid', async: false })
 class IsCardExpirationValidConstraint implements ValidatorConstraintInterface {
-  validate(value: string) {
+  /**
+   * Validate card expiration
+   */
+  public validate(value: string) {
     return isCardExpirationDateValid(value);
+  }
+
+  /**
+   * Returns default error message
+   */
+  public defaultMessage(): string {
+    return 'Card has expired.';
   }
 }
 
