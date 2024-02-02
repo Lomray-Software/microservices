a8c8d26acb812d0d79a7c0d022e1d469cb972f49
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2024-02-02 19:23:07 +0100

    feat(users): update change password tests

--- microservices/users/__tests__/config/remote.ts
+++ microservices/users/__tests__/config/remote.ts
@@ -12,6 +12,7 @@ describe('config/remote', () => {
 
   it('should correctly return cookies config: with remote', async () => {
     expect(await remoteConfig()).to.deep.equal({
+      changePasswordClearTokensType: CONST.MS_USER_CHANGE_PASSWORD_CLEAR_TOKENS_TYPE,
       passwordSaltRounds: CONST.MS_USER_PASSWORD_SALT_ROUNDS,
       removedAccountRestoreTime: CONST.MS_USER_REMOVE_ACCOUNT_RESTORE_TIME,
     });
--- microservices/users/__tests__/services/change-password-test.ts
+++ microservices/users/__tests__/services/change-password-test.ts
@@ -1,11 +1,13 @@
 import { TypeormMock } from '@lomray/microservice-helpers/mocks';
 import { waitResult } from '@lomray/microservice-helpers/test-helpers';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import User from '@entities/user';
 import UserRepository from '@repositories/user';
 import ChangePassword from '@services/change-password';
 
 describe('services/change-password', () => {
+  const sandbox = sinon.createSandbox();
   const repository = TypeormMock.entityManager.getCustomRepository(UserRepository);
   const userId = 'user-id';
   const newPassword = 'new-password';
@@ -24,65 +26,77 @@ describe('services/change-password', () => {
     TypeormMock.sandbox.reset();
   });
 
-  it('should throw error: user not found', async () => {
-    const service = ChangePassword.init({
-      userId,
-      repository,
-    });
-
-    TypeormMock.entityManager.findOne.resolves(undefined);
-
-    expect(await waitResult(service.change(newPassword, oldPassword))).to.throw('User not found');
+  afterEach(() => {
+    sandbox.reset();
   });
 
-  it('should throw error: oldPassword or confirmation not provided', async () => {
-    const service = ChangePassword.init({
-      userId,
-      repository,
+  describe('change', () => {
+    it('should throw error: user not found', async () => {
+      const service = ChangePassword.init({
+        userId,
+        repository,
+      });
+
+      TypeormMock.entityManager.findOne.resolves(undefined);
+
+      expect(await waitResult(service.change(newPassword, oldPassword))).to.throw('User not found');
     });
 
-    expect(await waitResult(service.change(newPassword))).to.throw(
-      'Either of confirm methods should be provided',
-    );
-  });
+    it('should throw error: oldPassword or confirmation not provided', async () => {
+      const service = ChangePassword.init({
+        userId,
+        repository,
+      });
 
-  it('should throw error: invalid old password', async () => {
-    const service = ChangePassword.init({
-      userId,
-      repository,
+      expect(await waitResult(service.change(newPassword))).to.throw(
+        'Either of confirm methods should be provided',
+      );
     });
 
-    TypeormMock.entityManager.findOne.resolves(mockUser);
+    it('should throw error: invalid old password', async () => {
+      const service = ChangePassword.init({
+        userId,
+        repository,
+      });
 
-    expect(await waitResult(service.change(newPassword, 'invalid-password'))).to.throw(
-      'Invalid old password',
-    );
-  });
+      TypeormMock.entityManager.findOne.resolves(mockUser);
 
-  it('should throw error: invalid confirmation', async () => {
-    const service = ChangePassword.init({
-      userId,
-      repository,
-      isConfirmed: () => false,
+      expect(await waitResult(service.change(newPassword, 'invalid-password'))).to.throw(
+        'Invalid old password',
+      );
     });
 
-    TypeormMock.entityManager.findOne.resolves(mockUser);
+    it('should throw error: invalid confirmation', async () => {
+      const service = ChangePassword.init({
+        userId,
+        repository,
+        isConfirmed: () => false,
+      });
 
-    expect(await waitResult(service.change(newPassword))).to.throw('Invalid confirmation code');
-  });
+      TypeormMock.entityManager.findOne.resolves(mockUser);
 
-  it('should successful change password', async () => {
-    const service = ChangePassword.init({
-      userId,
-      repository,
+      expect(await waitResult(service.change(newPassword))).to.throw('Invalid confirmation code');
     });
 
-    TypeormMock.entityManager.findOne.resolves(mockUser);
+    it('should successful change password', async () => {
+      const service = ChangePassword.init({
+        userId,
+        repository,
+      });
+      // Private method
+      // @ts-ignore
+      const handleClearUserTokensStub = sandbox.stub(service, 'handleClearUserTokens');
 
-    await service.change(newPassword, oldPassword);
+      TypeormMock.entityManager.findOne.resolves(mockUser);
 
-    const [, user] = TypeormMock.entityManager.save.firstCall.args;
+      await service.change(newPassword, oldPassword);
 
-    expect(repository.isValidPassword(user as User, newPassword)).to.true;
+      const [, user] = TypeormMock.entityManager.save.firstCall.args;
+      const [argUserId] = handleClearUserTokensStub.firstCall.args;
+
+      expect(repository.isValidPassword(user as User, newPassword)).to.true;
+      expect(handleClearUserTokensStub).to.calledOnce;
+      expect(argUserId).to.equal(userId);
+    });
   });
 });
